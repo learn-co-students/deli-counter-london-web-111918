92d15779987166a44d46cf49f318f17bfdaf7f6e
Jack Ling <j.lingyx@gmail.com>
2018-10-15 23:43:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+   
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+    
+  else
+    i = 0
+    position = 1
+    while i < katz_deli.length
+    person = katz_deli[i]
+    puts "The line is currently: #{position}. #{person}"
+    i += 1
+    position += 1
+    end
+  end
+end
+
+def take_a_number(katz_deli, name)
+  puts "#{position}. #{name}"
+  position += 1
+end
