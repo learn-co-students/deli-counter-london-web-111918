9e0376d578f0b19168e45eb46f736d7e687f0beb
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-11-16 10:37:52 +0000

    Now serving..

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line (a)
+  x = "The line is currently:"
+  if a.size == 0
+    puts "The line is currently empty."
+  else
+    a.each_with_index do |item, index|
+      x << " #{index + 1}. #{item}"
+    end
+  puts x
+end
+end
+
+
+def take_a_number (a, n)
+  a.push(n)
+  puts "Welcome, #{n}. You are number #{a.size} in line."
+end
+
+def now_serving (array)
+  if  array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift()}."
+  end
+end
